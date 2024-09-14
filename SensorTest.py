b999d939764bc2121c688f333354881ce48f363b
Jupiter <62066709+ItsJupiter21@users.noreply.github.com>
2024-09-14 23:42:42 +0300

    deployed for overnight with "on" filename
    wish me luck

--- SensorTest.py
+++ SensorTest.py
@@ -1,11 +1,15 @@
 
 # https://micropython-tsl2561.readthedocs.io/en/latest/tsl2561.html
 import tsl2561
-from machine import I2C, Pin
+from machine import I2C, Pin,PWM
 i2c = I2C(1, sda=Pin(14), scl=Pin(15))
 sensor = tsl2561.TSL2561(i2c)
 
+Led = PWM(Pin(28), freq=600000,invert=True)
+def LedB(B):
+    Led.duty_u16(int(65535*B))
 
+LedB(1)
 sensor.gain(16)#16 is the most sensitive
 
 #1:3918.05 max
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 
+
+#iav2x.csv have the LED at full brigthness fuck me
 from machine import Pin,PWM,I2C
 from time import sleep, time_ns
+import rp2
 
 from servo import Servo
 import tsl2561
 
+ttystart=False
 
 Led = PWM(Pin(28), freq=600000,invert=True)
 def LedB(B):
@@ -16,16 +20,25 @@ Servo.move(0)
 
 i2c = I2C(1, sda=Pin(14), scl=Pin(15))
 sensor = tsl2561.TSL2561(i2c)
-
+sensor.integration_time(402)
 sensor.gain(16)#16 is the most sensitive
-
 #1:3918.05 max
 #16:247.0598 max
 
 
-input("Press enter to start")#wait for tty to start
+times=[]
 
-for q in range(10):
+if ttystart==1:
+    input("Press enter to start")#wait for tty to start
+else:
+    while(rp2.bootsel_button()==0):
+        continue
+    sleep(10)
+tRef0=time_ns()
+
+
+for q in range(10):#amount of itterations
+    
     index=open('index.txt','a+')
     try:
         char=int(index.read())
@@ -36,7 +49,7 @@ for q in range(10):
     index.write(str(char+1))
     index.close()
     char=str(char)
-    file=open("iav2"+str(char)+'.csv','w')
+    file=open("on"+str(char)+'.csv','w')
 
     Servo.move(180)
     sleep(0.5)
@@ -50,19 +63,20 @@ for q in range(10):
 
         Servo.move(0)
         file.flush()
-        sleep(f/700)
+        sleep(f/800)
         Servo.move(A)
         sleep(f/300+0.05)
         print(A)
         for i in range(0,21,1):
             B=i/20
             LedB(B)
-            sleep(0.15)
-            L=sensor.read()#no autogain, calibrated out
+            sleep(0.02)
+            for i in range(4):
+                L=sensor.read()#no autogain, calibrated out
             
-            out=[B,L,A]
-            print(B,L) 
-            file.write(','.join(map(str, out))+'\n')
+                out=[B,L,A]
+                print(B,L) 
+                file.write(','.join(map(str, out))+'\n')
             
         LedB(0)
             
@@ -70,6 +84,10 @@ for q in range(10):
     t= 0.000000001*(time_ns()-tRef)
 
     print(t)
+    times.append(t)
 
-#500.3311
-#394.0228
+t0= 0.000000001*(time_ns()-tRef0)
+print(tdef)
+print(times)
+
+
