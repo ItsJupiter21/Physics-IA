a74535d88df76a563bff6ad50145cab0d92b4e4c
Jupiter <62066709+ItsJupiter21@users.noreply.github.com>
2024-09-15 19:10:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #iav2x.csv have the LED at full brigthness fuck me
 from machine import Pin,PWM,I2C
 from time import sleep, time_ns
-import rp2,ws2812
+import rp2, ws2812
 
 from servo import Servo
 import tsl2561
@@ -15,17 +15,21 @@ brightness=1
 
 ttystart=False
 
+
 Led = PWM(Pin(28), freq=600000,invert=True)
 def LedB(B):
     Led.duty_u16(int(65535*B))
-
+    
+    
 def ledw(r,g,b):
     r=round(r*brightness)
     g=round(g*brightness)
     b=round(b*brightness)
     ledsm.put((g<<24) | (r<<16) | (b<<8))
 
-Servo = Servo(pin=29,)    
+
+Servo = Servo(pin=29,)
+Servo.move(0)
 
 i2c = I2C(1, sda=Pin(14), scl=Pin(15))
 sensor = tsl2561.TSL2561(i2c)
@@ -35,11 +39,15 @@ sensor.gain(16)#16 is the most sensitive
 #16:247.0598 max
 
 
+ttystart=False
+rep=1 #amount to repeat
+anglestep=2 #angle change per ittetation, 2 in tests
+ledvalues=10 #amount of LED itterations, 20 in tests
+readings=1
+MaxB=0.5
+
+
 times=[]
-
-
-Servo.move(0)
-ledw(0,0,255)
 
 if ttystart==1:
     input("Press enter to start")#wait for tty to start
@@ -47,10 +55,12 @@ else:
     while(rp2.bootsel_button()==0):
         continue
     ledw(0,0,0)
+    print("starting")
     sleep(10)
 tRef0=time_ns()
 
-for q in range(2):#amount of itterations
+
+for q in range(rep):#amount of itterations
     
     index=open('index.txt','a+')
     try:
@@ -71,7 +81,7 @@ for q in range(2):#amount of itterations
 
 
     tRef=time_ns()
-    for f in range(0,191,2):
+    for f in range(0,191,anglestep):
         A=f
 
         Servo.move(0)
@@ -80,12 +90,12 @@ for q in range(2):#amount of itterations
         Servo.move(A)
         sleep(f/300+0.05)
         print(A)
-        for i in range(0,21,1):
-            B=i/20
+        for i in range(0,ledvalues+1,1):
+            B=(i/ledvalues)*MaxB
             LedB(B)
             sleep(0.02)
-            for i in range(4):
-                L=sensor.read()#no autogain, calibrated out
+            for i in range(readings):
+                L=sensor.read()# no autogain, calibrated out
             
                 out=[B,L,A]
                 print(B,L) 
@@ -102,13 +112,11 @@ for q in range(2):#amount of itterations
 
 
 t0= 0.000000001*(time_ns()-tRef0)
-print(tdef)
+print(t0)
 print(times)
-Servo.move(0)
-ledw(255,0,0)
-ledB(0)
-while(1):
-    ledw(255,0,0)
+
+while True:
+    LedB(1)
     sleep(0.5)
-    ledw(0,0,255)
+    LedB(0)
     sleep(0.5)
